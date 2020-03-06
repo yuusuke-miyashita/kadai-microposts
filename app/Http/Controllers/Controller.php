5556f025e0c13025e847a329ae81d4d87cca15eb
yuusuke.miyashita@m08035266125@yahoo.co.jp <yuusuke.6125@outlook.jp>
2020-03-06 13:33:35 +0000

    follow, unfollow

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,25 +10,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+
     public function counts($user) {
         $count_microposts = $user->microposts()->count();
+        $count_followings = $user->followings()->count();
+        $count_followers = $user->followers()->count();
 
         return [
             'count_microposts' => $count_microposts,
+            'count_followings' => $count_followings,
+            'count_followers' => $count_followers,
         ];
     }
-    public function show($id)
-    {
-        $user = User::find($id);
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
-
-        $data = [
-            'user' => $user,
-            'microposts' => $microposts,
-        ];
-
-        $data += $this->counts($user);
-
-        return view('users.show', $data);
-    }
+    
 }
